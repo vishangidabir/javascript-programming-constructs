e8bef8e3b2e8294822568f3a404b190a38a702b6
vishangi <vishangid@gmail.com>
2022-07-30 00:46:59 +0530

    Added : Read a single digit number and write the number in word

---
+++ DigitNumber.js
@@ -0,0 +1,21 @@
+let number = 3;
+if(number == 1)
+console.log("One");
+else if(number == 2)
+console.log("Two");
+else if(number == 3)
+console.log("Three");
+else if(number == 4)
+console.log("Four");
+else if(number == 5)
+console.log("Five");
+else if(number == 6)
+console.log("Six");
+else if(number == 7)
+console.log("Seven");
+else if(number == 8)
+console.log("Eight");
+else if(number == 9)
+console.log("Nine");
+else
+console.log("Zero");
