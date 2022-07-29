66848b3baa58973842397294c7bc5a5420b6f57a
vishangi <vishangid@gmail.com>
2022-07-30 00:57:02 +0530

    Added : Read a Number 1, 10, 100, 1000, etc and display unit, ten, hundred..

---
+++ DisplayUnit.js
@@ -0,0 +1,11 @@
+let number = 100;
+if(number == 1)
+console.log("One");
+else if(number == 10)
+console.log("Ten");
+else if(number == 100)
+console.log("Hundred");
+else if(number == 1000)
+console.log("Thousand");
+else if(number == 10000)
+console.log("Ten Thousand ");
