b76cae8818789db19b373612911719c825462e1d
vishangi <vishangid@gmail.com>
2022-07-30 00:52:44 +0530

    Added : Read a Number and Display the week day (Sunday, Monday,...)

---
+++ DisplayWeek.js
@@ -0,0 +1,15 @@
+let day = 3;
+if(day == 1)
+console.log("Sunday");
+else if(day == 2)
+console.log("Monday");
+else if(day == 3)
+console.log("Tuesday");
+else if(day == 4)
+console.log("Wenesday");
+else if(day == 5)
+console.log("Tursday");
+else if(day == 6)
+console.log("Friday");
+else 
+console.log("Saturday");
